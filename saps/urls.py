2690cb51eb3275cc2521048c00892cf74a9ae56a
Jakob Helmecke <jakob.helmecke@yeebase.com>
2020-02-27 16:03:33 +0100

    Move sipgate to seperate app

--- saps/urls.py
+++ saps/urls.py
@@ -1,13 +1,8 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from . import views
-
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login', views.login, name='login'),
-    path('authorize', views.authorize, name='authorize'),
-    path('me', views.me, name='me'),
+    path('', include('sipgate.urls')),
     path('snom/', include('snom.urls')),
     path('admin/', admin.site.urls),
 ]
--- sipgate/models.py
+++ sipgate/models.py
@@ -1,26 +1,6 @@
-import time
+from time import localtime, strftime
 from django.db import models
 from django.contrib.auth.models import User
-
-
-class Phone(models.Model):
-    phone_type = models.CharField(max_length=12)
-    mac_address = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.phone_type + '-' + self.mac_address
-
-
-class Device(models.Model):
-    username = models.CharField(max_length=80)
-    password = models.CharField(max_length=80)
-    sip_server = models.CharField(max_length=80)
-    outbound_proxy = models.CharField(max_length=80)
-
-
-class Assignments(models.Model):
-    phone = models.ForeignKey(Phone, on_delete=models.CASCADE)
-    device = models.ForeignKey(Device, on_delete=models.CASCADE)
 
 
 class OAuth2Token(models.Model):
@@ -36,7 +16,7 @@ class OAuth2Token(models.Model):
         verbose_name_plural = 'OAuth2Tokens'
 
     def __str__(self):
-        return self.user.email + '-' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(self.expires_at))
+        return self.user.email + '-' + strftime('%Y-%m-%d %H:%M:%S', localtime(self.expires_at))
 
     def to_token(self):
         return dict(
