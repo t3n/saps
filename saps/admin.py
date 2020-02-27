a20f2d9e3a1696bdb1d8d34fc9aaa1e6386c2a95
Jakob Helmecke <jakob.helmecke@yeebase.com>
2020-02-27 15:21:46 +0100

    Move snom to seperate app

--- saps/admin.py
+++ saps/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 
-from .models import Assignments, Device, Phone, OAuth2Token
+from .models import OAuth2Token
 
-admin.site.register(Assignments)
-admin.site.register(Device)
-admin.site.register(Phone)
 admin.site.register(OAuth2Token)
---
+++ snom/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import Phone
+
+admin.site.register(Phone)
