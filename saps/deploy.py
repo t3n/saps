907908659b2adf87a64ca2e6044d258c35e9c8b9
Max Schmidt <m.schmidt@yeebase.com>
2020-03-09 08:09:10 +0100

    Add Docker and psql as backend (#12)

---
+++ saps/deploy.py
@@ -0,0 +1,16 @@
+import os
+
+from .settings import *   # noqa: F403
+
+# This is NOT a complete production settings file. For more, see:
+# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+
+DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+ALLOWED_HOSTS = ['localhost']
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # noqa: F405
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
